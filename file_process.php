cdfc62120ae19bbb5e1814c604c8c5cee572f19f
Устинов Вадим <vvadi12111@gmail.com>
2025-05-07 10:26:09 +0500

    всё нормально

---
+++ file_process.php
@@ -0,0 +1,12 @@
+<?php
+
+if (file_exists('example.txt')) {
+    $file = fopen('example.txt', 'r');
+    
+    while (($line = fgets($file)) !== false) {
+        print("{$line}<br>");
+    }
+    fclose($file);
+} else {
+    print('Файл не найден.');
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	include 'header.php';
+?>
+
+<main>
+    <p>Это главная страница.</p>
+</main>
+
+<?php
+	include 'footer.php';
+?>
